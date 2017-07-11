09bad79836e9b089ea01d4ecde2d373ba54142ea
Kavita <kavi.b0208@gmail.com@github.com>
2017-07-11 14:23:12 +0100

    initial commit

---
+++ reducer/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  stock: {
+    'A1': {
+      name: 'MarsBar',
+      quantity: 10,
+      price: 0.85
+    },
+    'A2': {
+      name: 'Kettle Chips',
+      quantity: 10,
+      price: 0.90
+    },
+    'A3': {
+      name: 'Kitkat',
+      quantity: 10,
+      price: 0.75
+    }
+  },
+  credit : [],
+  change: {},
+  float : {
+    '0.10': 5,
+    '0.20': 5,
+    '0.50': 10,
+    '1': 20
+  },
+  displayMessage: '',
+  selection : '',
+  productDispenser : '',
+  dispenserDoorOpen: false,
+  power: true
+};
